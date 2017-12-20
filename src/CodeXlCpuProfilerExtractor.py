848cd3c3ece2db68d51ff78b62b7b135ce5be9c4
unknown <dws@yoyoma.com>
2017-12-20 17:32:49 -0500

    Reworked the script so that it handles a folder of .csv files instead of an individual file.

--- src/CodeXlCpuProfilerExtractor.py
+++ src/CodeXlCpuProfilerExtractor.py
@@ -5,54 +5,68 @@ Created on Dec 19, 2017
 '''
 import csv
 import getopt
+import os
 import re
 import sys
 from _ast import arg
 
 def main(argv):
-    input_csv = None
+    input_csv_folder = None
     output_csv = None
     try:
         opts, args = getopt.getopt(argv,"hi:o:")
     except:
-        print('CodeXlCpuProfilerExtractor.py -i <input csv path> -o <output csv path>')
+        print('CodeXlCpuProfilerExtractor.py -i <input csv folder path> -o <output csv path>')
         sys.exit(2)
         
     for opt, arg in opts:
         if opt == '-h':
-            print('CodeXlCpuProfilerExtractor.py -i <input csv path> -o <output csv path>')
+            print('CodeXlCpuProfilerExtractor.py -i <input csv folder path> -o <output csv path>')
             sys.exit()
         elif opt in ("-i"):
-            input_csv = arg
+            input_csv_folder = arg
         elif opt in ("-o"):
             output_csv = arg
             
-    if not input_csv or not output_csv:
-        print('CodeXlCpuProfilerExtractor.py -i <input csv path> -o <output csv path>')
+    if not input_csv_folder or not output_csv:
+        print('CodeXlCpuProfilerExtractor.py -i <input csv folder path> -o <output csv path>')
         sys.exit(2)
         
-    duration = None
-    with open(input_csv) as f:
-        reader = csv.reader(f)
-    
-        for row in reader:
-            if 'Profile Duration:' in row:
-                duration = row
-                break
+    # Get all of the .csv file names.
+    csv_files = [filename for filename in os.listdir(input_csv_folder) if filename.endswith(".csv")]
         
-        if duration:
-            # Expecting the string to look like this: ['Profile Duration:', '74 seconds']
-            duration_time = re.search("^.*', '([0-9]+).*$", str(duration))
-            duration_unit = re.search("^.*', '[0-9]+ ([a-zA-Z]+)'.*$", str(duration))
+    collected_data = {}
+    for input_csv_filename in csv_files:
+        duration = None
+        input_csv_filepath = input_csv_folder + '/' + input_csv_filename
+        with open(input_csv_filepath) as f:
+            reader = csv.reader(f)
+    
+            for row in reader:
+                if 'Profile Duration:' in row:
+                    duration = row
+                    break
+        
+            if duration:
+                # Expecting the string to look like this: ['Profile Duration:', '74 seconds']
+                duration_time = re.search("^.*', '([0-9]+).*$", str(duration))
 
-            if duration_time:
-                with open(output_csv, 'w', newline='') as csvfile:
-                    fieldnames = ['Duration', 'Unit']
-                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                    writer.writeheader()
-                    writer.writerow({'Duration': duration_time.group(1), 'Unit': duration_unit.group(1)})
-        else:
-            sys.exit("Profile Duration was not found in the input .csv.  No output .csv was created.")
-                    
+                if duration_time:
+                    filename = os.path.splitext(input_csv_filename)[0]
+                    collected_data[filename] = duration_time.group(1)
+            else:
+                sys.exit("Profile Duration was not found in the input .csv.  No output .csv was created.")
+    
+    if collected_data:
+        fieldnames = []            
+        with open(output_csv, 'w', newline='') as csvfile:
+            for key, value in collected_data.items():
+                fieldnames.append(key)
+                
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+                
+            writer.writerow(collected_data)    
+
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
